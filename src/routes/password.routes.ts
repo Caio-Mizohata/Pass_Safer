3a52e8c94d218ca7367965c185e466efb900fad5
Caio-Mizohata <caiomizohata@gmail.com>
2026-05-20 18:44:27 -0300

    refactor: remover validação de chaves permitidas das rotas de senha

--- src/routes/password.routes.ts
+++ src/routes/password.routes.ts
@@ -1,14 +1,13 @@
 import { Router } from "express";
 import { PasswordController } from "../controllers/password.controller.ts";
-import { PASSWORD_ALLOWED_KEYS, validateAllowedKeys } from "../middlewares/validadeKeys.middleware.ts";
 import { passwordLimiter } from "../middlewares/rateLimit.middleware.ts";
 
 const PassRouter = Router();
 
-PassRouter.post('/api/v1/passwords', passwordLimiter, validateAllowedKeys(PASSWORD_ALLOWED_KEYS), PasswordController.savePassword);
+PassRouter.post('/api/v1/passwords', passwordLimiter, PasswordController.savePassword);
 PassRouter.get('/api/v1/passwordslist', passwordLimiter, PasswordController.getAllPasswords);
 PassRouter.get('/api/v1/passwordslist/:id', passwordLimiter, PasswordController.getUserPassword);
-PassRouter.put('/api/v1/passwordslist/:id', passwordLimiter, validateAllowedKeys(PASSWORD_ALLOWED_KEYS), PasswordController.updatePassword);
+PassRouter.put('/api/v1/passwordslist/:id', passwordLimiter, PasswordController.updatePassword);
 PassRouter.delete('/api/v1/passwordslist/:id', passwordLimiter, PasswordController.deletePassword);
 
 export default PassRouter;
